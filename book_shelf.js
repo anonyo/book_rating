f205df4a0dae04e319048c47ce598ffc14066087
Anonyo Hossain <anonyo.hossain@gmail.com>
2014-05-08 15:07:24 +0000

    first commit

---
+++ book_shelf.js
@@ -0,0 +1,64 @@
+Books = new Meteor.Collection("books");
+
+Meteor.methods({
+  upvote: function (bookId) {
+  	var book = Books.findOne(bookId);
+  	Books.update(bookId, 
+  				  {$set: {votes: book.votes + 1}}
+  );				  
+  }, 
+    downvote: function (bookId) {
+  	var book = Books.findOne(bookId);
+  	Books.update(bookId, 
+  				  {$set: {votes: book.votes - 1}}
+  );				  
+  }
+
+});
+
+if (Meteor.isServer) {
+  
+    Books.allow({
+      insert: function (userId, book) {
+        return book.votes === 0; 
+      }
+    
+    });
+
+  Meteor.publish("books", function() {
+    return Books.find();
+  });
+
+} else if (Meteor.isClient) {
+  
+
+
+    Meteor.subscribe("books");
+    Template.booksList.books = function() {
+      return Books.find({}, { sort: {votes: -1}}); 
+    };
+
+   Template.booksList.events({
+      "click .upvote": function() {
+      	Meteor.call("upvote", this._id);
+      }, 
+        "click .downvote": function() {
+      	Meteor.call("downvote", this._id);
+      }
+   });
+
+  Template.newBook.events({
+     "submit .newBookForm": function (evt) {
+     	evt.preventDefault();
+     	Books.insert({
+          title: $(".title").val(),
+          author: $(".author").val(), 
+          votes: 0
+     	}, function (err) {
+     		if (! err)
+     			$("input[type=text]").val();
+     	});
+     }
+   });
+
+}
